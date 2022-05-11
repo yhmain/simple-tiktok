25746b265c20534655b7d4b9a71f7a0f411f7778
yhmain <yhmain@gmail.com>
2022-05-11 10:58:44 +0800

    d

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ func main() {
 
 	initRouter(r)
 
-	// 命令行运行：go build && ./simple-demo
+	// 命令行运行：go build && ./simple-tiktok
 	r.Run("192.168.1.106:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
