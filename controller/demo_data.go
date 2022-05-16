2939160f6fd1e6d51d32562a1b297a1bf4921084
yhmain <yhmain@gmail.com>
2022-05-16 14:58:16 +0800

    main test

--- controller/demo_data.go
+++ controller/demo_data.go
@@ -3,11 +3,29 @@ package controller
 var DemoVideos = []Video{
 	{
 		Id:            1,
-		Author:        DemoUser,
+		User:          DemoUsers[0],
 		PlayUrl:       "https://www.w3schools.com/html/movie.mp4",
 		CoverUrl:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
 		FavoriteCount: 0,
 		CommentCount:  0,
+		IsFavorite:    false,
+	},
+	{
+		Id:            2,
+		User:          DemoUsers[1],
+		PlayUrl:       "https://www.w3schools.com/html/movie.mp4",
+		CoverUrl:      PREFIX_COVERS + "bear.jpg",
+		FavoriteCount: 2,
+		CommentCount:  2,
+		IsFavorite:    false,
+	},
+	{
+		Id:            3,
+		User:          DemoUsers[2],
+		PlayUrl:       "https://www.w3schools.com/html/movie.mp4",
+		CoverUrl:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
+		FavoriteCount: 3,
+		CommentCount:  3,
 		IsFavorite:    false,
 	},
 }
@@ -15,16 +33,32 @@ var DemoVideos = []Video{
 var DemoComments = []Comment{
 	{
 		Id:         1,
-		User:       DemoUser,
+		User:       DemoUsers[0],
 		Content:    "Test Comment",
 		CreateDate: "05-01",
 	},
 }
 
-var DemoUser = User{
-	Id:            1,
-	Name:          "TestUser",
-	FollowCount:   0,
-	FollowerCount: 0,
-	IsFollow:      false,
+var DemoUsers = []User{
+	{
+		Id:            1,
+		Name:          "TestUser",
+		FollowCount:   0,
+		FollowerCount: 0,
+		IsFollow:      false,
+	},
+	{
+		Id:            2,
+		Name:          "HaHaUser",
+		FollowCount:   2,
+		FollowerCount: 2,
+		IsFollow:      false,
+	},
+	{
+		Id:            3,
+		Name:          "WoDeUser",
+		FollowCount:   3,
+		FollowerCount: 3,
+		IsFollow:      false,
+	},
 }
