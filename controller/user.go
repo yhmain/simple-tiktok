519693c51919c5fcc2fed19f997111db6e95a4c9
yhmain <yhmain@gmail.com>
2022-05-17 21:59:45 +0800

    a

--- controller/user.go
+++ controller/user.go
@@ -12,27 +12,6 @@ import (
 	"github.com/yhmain/simple-tiktok/service"
 )
 
-// usersLoginInfo use map to store user info, and key is username+password for demo
-// user data will be cleared every time the server starts
-// test data: username=zhanglei, password=douyin
-// 准备删除的
-// var usersLoginInfo = map[string]User{
-// 	"zhangleidouyin": {
-// 		Id:            1,
-// 		Name:          "zhanglei",
-// 		FollowCount:   10,
-// 		FollowerCount: 5,
-// 		IsFollow:      true,
-// 	},
-// 	"admin123456": {
-// 		Id:            2,
-// 		Name:          "admin",
-// 		FollowCount:   13,
-// 		FollowerCount: 51,
-// 		IsFollow:      true,
-// 	},
-// }
-
 //用户登录响应体
 type UserLoginResponse struct {
 	Response
